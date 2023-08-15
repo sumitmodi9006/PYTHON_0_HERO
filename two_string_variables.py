5e57ddee6dcb584054db669a40e63322fe949ea8
sumitmodi9006 <140099587+sumitmodi9006@users.noreply.github.com>
2023-08-16 02:19:10 +0530

    Update two_string_variables.py

--- two_string_variables.py
+++ two_string_variables.py
@@ -4,3 +4,4 @@ print(a+b)
 print(a+" "+b)#if you want add space in string variabe use like space in semicolon
 c=30
 print(c+30)
+#print(a+c)#this is error we add only string with string and int with int .
