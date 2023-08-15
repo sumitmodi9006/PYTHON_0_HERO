4e017f71a99e1eca9053edcc3154e8f10505a248
sumitmodi9006 <140099587+sumitmodi9006@users.noreply.github.com>
2023-08-16 03:05:00 +0530

    Create operator_assignment.py

---
+++ operator_assignment.py
@@ -0,0 +1,12 @@
+#ASSIGNMENT OPERATORS
+x=5#equal to variable ko assigned krne ka kaam aata hai 
+print(x)
+x=x+5#INCREMENT OPERATOR =X+
+print(x) # x ka value 10 hai
+x+=5
+print(x) #OR YAHA PE X=10 THA TB X=5+10=15  AYA ANWSER
+
+x=x-5# decreament process
+printf(x)
+x-=5
+print(x)
