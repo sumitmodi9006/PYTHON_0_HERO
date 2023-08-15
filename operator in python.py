fa74c2305e9aa51052528b549b09fac9bd7af0af
sumitmodi9006 <140099587+sumitmodi9006@users.noreply.github.com>
2023-08-16 02:43:06 +0530

    Create operator1 in python.py

---
+++ operator in python.py
@@ -0,0 +1,18 @@
+#using ARITHEMATIC OPERATOR
+a=10
+b=30
+c=a+b#ADDITION
+d=a-b#SUBTRACTIO
+e=a*b#MULTIPLICATION
+f=a/b#DIVISION
+g=a%b#MODULUS =remainder value
+h=a**b#EXPONENT=10**30  mtlb sidha mai 10 **2 =100 uska power se utna baar multiply krega 10**3=1000
+i=a//b#FLOOR DIVISION iska kaam integer mai dekha or .ke baad wala sb hatta dega 10/3=3.3333 aisa rahega but iska mai 10//3=3 only int value 
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+
